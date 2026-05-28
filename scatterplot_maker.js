4baf2a34eec0adb091a0ec17543f6836e95f7885
theofficialklpiano <kenneth.h.liu@gmail.com>
2026-05-27 20:44:05 -0400

    Update scatterplot_maker.js

--- scatterplot_maker.js
+++ scatterplot_maker.js
@@ -2,21 +2,27 @@ function makePlot() {
 
     try {
 
-        // Get input values
+        // get input
         let xText = document.getElementById("x-values").value;
         let yText = document.getElementById("y-values").value;
 
-        // Convert strings into arrays
+        // string -> array
         let xValues = xText.split(",").map(Number);
         let yValues = yText.split(",").map(Number);
 
-        // Check lengths
+        // check lengths
         if (xValues.length !== yValues.length) {
             alert("X and Y must have same number of values.");
             return;
         }
 
-        // Create scatterplot
+        // check if number
+        if (xValues.some(isNaN) || yValues.some(isNaN)) {
+            alert("Please enter only numbers.");
+            return;
+        }
+
+        // create scatterplot
         let trace = {
             x: xValues,
             y: yValues,
